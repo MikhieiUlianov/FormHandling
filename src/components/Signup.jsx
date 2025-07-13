5d66bff397fa967f81bc6c1fd89bb8b8ab85180e
Mkh <example@gmail.com>
2025-07-13 08:56:13 +0200

    keep-values-on-error

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,13 +7,6 @@ import {
 import { useActionState } from "react";
 
 export default function Signup() {
-  //when we use "useActionState" our fucntion is called in a different way,
-  //in this case formData object will be as the second arg.
-  //first arg is the previous state
-
-  //first time it is executed,
-  // our initial state will simply be that initial state,
-  //  which was passed to this hook
   function signUpAction(prevState, formData) {
     const email = formData.get("email");
     const password = formData.get("password");
@@ -50,19 +43,24 @@ export default function Signup() {
     }
 
     if (errors.length > 0) {
-      return { errors };
+      return {
+        errors,
+        enteredValues: {
+          email,
+          password,
+          confirmPassword,
+          firstName,
+          lastName,
+          role,
+          acquisitionChannel,
+          terms,
+        },
+      };
     }
 
     return { errors: null };
   }
-  //second argument is an initial state,
-  // which will be active while "signUpAction" function will not be executed
-
-  //arg #1 it is state which can change from initial to state which will be returned by "signUpAction"
-  //arg #2 we get an updated form action.
-  // which is wrappin our "signUpAction" function, and does so it can listen it, and provides some exctra futures.
-  //arg #3 pending status, true or false
-  const [formState, formAction /* pending */] = useActionState(signUpAction, {
+  const [formState, formAction] = useActionState(signUpAction, {
     errors: null,
   });
 
@@ -73,13 +71,23 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          defaultValue={formState.enteredValues?.email}
+        />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            defaultValue={formState.enteredValues?.password}
+          />
         </div>
 
         <div className="control">
@@ -88,6 +96,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
           />
         </div>
       </div>
@@ -97,18 +106,32 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input
+            type="text"
+            id="first-name"
+            name="first-name"
+            defaultValue={formState.enteredValues?.firstName}
+          />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input
+            type="text"
+            id="last-name"
+            name="last-name"
+            defaultValue={formState.enteredValues?.lastName}
+          />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select
+          id="role"
+          name="role"
+          defaultValue={formState.enteredValues?.role}
+        >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -125,6 +148,9 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes(
+              "google"
+            )}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -135,20 +161,36 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes(
+              "friend"
+            )}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input
+            type="checkbox"
+            id="other"
+            name="acquisition"
+            value="other"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes(
+              "other"
+            )}
+          />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            defaultChecked={formState.enteredValues?.terms}
+          />
+          I agree to the terms and conditions
         </label>
       </div>
       {formState.errors && (
